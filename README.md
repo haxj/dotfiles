02ebf27e2e395b597e6fd7722bce1e985deeb9dc
haxj <22296968+haxj@users.noreply.github.com>
2024-05-25 00:56:19 +0700

    config: Add Neovim NvChad 2.5

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dotfiles
+
+## Install
+
+### NvChad 2.5
+
+```
+mv ~/.config/nvim ~/.config/nvim_backup
+mv ~/.local/share/nvim ~/.local/share/nvim_backup
+cp -r nvim_nvchad_2.5 ~/.config/nvim
+```
+
---
+++ nvim_nvchad_2.5/lua/plugins/init.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
